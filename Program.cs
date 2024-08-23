c77cd350e73a354f5ccdb4b07b81651bf8d23ba9
Marceloney <marcelodevjr@gmail.com>
2024-08-23 13:47:00 -0300

    atualizando com Enumerador

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Security.Cryptography.X509Certificates;
 
 namespace aula
 {
@@ -7,29 +8,34 @@ namespace aula
         static void Main(string[] args)
         {
             
-            Product mouse = new Product(1, "Mouse Gamer", 299.97);
-
+            Product mouse = new Product(1, "Mouse Gamer", 299.97, EproductType.Product);
+            var manutecaoEletrica = new Product(2, "Manutenção Elétrica residencial", 500, EproductType.Product);
             mouse.Id = 55;
 
             Console.WriteLine(mouse.Id);
             Console.WriteLine(mouse.Name);
             Console.WriteLine(mouse.Price);
+            Console.WriteLine(mouse.Type);
+            Console.WriteLine((int)mouse.Type);
 
         }
 
 
         struct Product
         {
-            public Product(int id, string name, double price)
+            public Product(int id, string name, double price, EproductType type)
             {
                 Id = id;
                 Name = name;
                 Price = price;
+                Type = type;
+                
 
             }
             public int Id;
             public string Name;
             public double Price;
+            public EproductType Type;
 
             public double PriceInDolar(double dolar)
             {
@@ -37,6 +43,11 @@ namespace aula
             }
         }
 
+        enum EproductType
+        {
+            Product = 1,
+            Service = 2
+        }
 
     }
 
